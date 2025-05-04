f14865bd51fd0931e1f8d0d5ff291a9a99fb6f16
Rodrigo Llull Torres <rllullt@gmail.com>
2025-05-04 18:23:07 -0400

    Fix send file button

--- src/pages/Dashboard/CampaignsPage.tsx
+++ src/pages/Dashboard/CampaignsPage.tsx
@@ -5,6 +5,17 @@ function CampaignsPage() {
   const [preview, setPreview] = useState<string | null>(null);
   const [status, setStatus] = useState<'idle' | 'loading' | 'success' | 'error'>('idle');
 
+  type UploadResponse = {
+    message: string;
+    metadata: {
+      filename: string;
+      content_type: string;
+      size_bytes: number;
+    };
+  };
+
+  const [responseData, setResponseData] = useState<UploadResponse | null>(null);
+  
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0] || null;
     if (file) {
@@ -25,13 +36,22 @@ function CampaignsPage() {
     console.log("API URL:", apiUrl);
 
     try {
-      const response = await fetch(`${apiUrl}/upload`, {
+      const response = await fetch(`${apiUrl}/campaigns/new/`, {
         method: 'POST',
         body: formData,
+        headers: {
+          'Accept': 'application/json'  // key: this tells "I wait JSON, no HTML"
+        }
       });
 
       if (response.ok) {
         setStatus('success');
+        response.json().then(data => {
+          console.log('Response data:', data);
+          setResponseData(data);
+        }).catch(error => {
+          console.error('Error parsing JSON:', error);
+        });
       } else {
         throw new Error('Error en la respuesta del backend');
       }
@@ -70,6 +90,11 @@ function CampaignsPage() {
         {status === 'success' && (
           <div className="mt-4 alert alert-success shadow-sm">
             Image processed successfully!
+            <br />Image data:
+            <br />FileName: {responseData?.metadata.filename}
+            <br />Content Type: {responseData?.metadata.content_type}
+            <br />Size: {responseData?.metadata.size_bytes} bytes
+            <br />This campaign image DOES cumply with all the requirements.
           </div>
         )}
         {status === 'error' && (
