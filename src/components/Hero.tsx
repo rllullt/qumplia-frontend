90c7a8f22628773429fe371c9a049dbfcbe229f8
Rodrigo Llull Torres <rllullt@gmail.com>
2025-05-12 10:53:56 -0400

    Modularized Hero Component + Fixed Landing images + «about us» field

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,24 +1,41 @@
-import HeroImage from '../assets/hero-img-1.png';
+import React from 'react';
 
-function Hero() {
+interface HeroProps {
+  title: string;
+  subtitle: string;
+  buttonText: string;
+  onButtonClick: () => void; // Example of a potential button click handler
+  largeImageUrl: string;
+  largeImageOnLeft?: boolean;
+}
+
+const Hero: React.FC<HeroProps> = ({
+  title,
+  subtitle,
+  buttonText,
+  onButtonClick,
+  largeImageUrl,
+  largeImageOnLeft = false,
+}) => {
+  const largeImageOrderClass = largeImageOnLeft ? 'lg:flex-row' : 'lg:flex-row-reverse';
   return (
     <div className="hero min-h-screen bg-base-200 pt-24 px-4 overflow-hidden">
-      <div className="hero-content flex-col-reverse lg:flex-row-reverse items-center gap-10">
+      <div className={`hero-content flex-col-reverse ${largeImageOrderClass} items-center gap-10`}>
         <div className="w-full lg:w-1/2">
           <img
-            src={HeroImage}
+            src={largeImageUrl}
             className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg rounded-lg shadow-2xl mx-auto"
-            alt="Imagen de Compliance"
+            alt="Hero Image"
           />
         </div>
         <div className="text-center lg:text-left w-full lg:w-1/2">
-          <h1 className="text-4xl sm:text-5xl font-bold">Compliance sin complicaciones</h1>
-          <p className="py-6 text-base sm:text-lg">Facilitamos el proceso de compliance en la industria financiera a través de IA y Blockchain.</p>
-          <button className="btn btn-primary rounded-full">Agenda una demo</button>
+          <h1 className="text-4xl sm:text-5xl font-bold">{title}</h1>
+          <p className="py-6 text-base sm:text-lg">{subtitle}</p>
+          <button className="btn btn-primary rounded-full" onClick={onButtonClick}>{buttonText}</button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,15 @@
 import BaseNavbar from '../components/BaseNavbar';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import HeroShield from '../assets/hero-img-shield.png';
+import HeroBargraph from '../assets/hero-img-bargraph.png';
 
 function LandingPage() {
+
+  const handleDemoClick = () => {
+    alert('Agenda una demo!');
+  };
+
   return (
     <div>
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
@@ -10,7 +17,21 @@ function LandingPage() {
             <Navbar />
           </BaseNavbar>
         </div>
-      <Hero />
+      <Hero
+        title="Compliance sin complicaciones"
+        subtitle="Facilitamos el proceso de compliance en la industria financiera a través de IA y Blockchain."
+        buttonText="Agenda una demo"
+        onButtonClick={handleDemoClick}
+        largeImageUrl={HeroShield}
+      />
+      <Hero
+        title="Optimiza el cumplimiento normativo en la industria financiera"
+        subtitle="Los errores de compliance en promociones financieras ponen en riesgo tu negocio. QumplIA ofrece una plataforma unificada potenciada por IA y Blockchain para asegurar el cumplimiento regulatorio, facilitando la identificación y corrección de problemas en cada etapa del ciclo de vida de la campaña."
+        buttonText="Agenda una demo"
+        onButtonClick={handleDemoClick}
+        largeImageUrl={HeroBargraph}
+        largeImageOnLeft={true}
+      />
       {/* Aquí puedes añadir más secciones de tu landing page */}
     </div>
   );
