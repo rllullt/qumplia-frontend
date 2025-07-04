a886ab0b1569303511e1c508ed5d1aa474e14f46
Rodrigo Llull Torres <rllullt@gmail.com>
2025-07-04 18:25:47 -0400

    fix Vara wallect-connect: package.json and vite.config.ts for wallet-connect support

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
+import { checker } from 'vite-plugin-checker';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    nodePolyfills(),
+    svgr(),
+    checker({
+      typescript: true, 
+    }),
   ],
   base: '/',
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
